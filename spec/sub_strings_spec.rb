847f88f8853f0d9971ed7e708e6d748b3e36935f
Razvan Rotundu <raz.rotundu@gmail.com>
2024-08-09 15:02:48 -0400

    initial commit

---
+++ spec/sub_strings_spec.rb
@@ -0,0 +1,25 @@
+# Razvan Rotundu 2024 
+# Sub Strings test cases
+
+
+require_relative "../project/sub_strings"
+
+describe "Sub strings project testing" do
+
+  it "Correctly identifies if a substring is present" do
+
+  end
+
+  it "Correctly identifies multiple substrings present" do
+
+  end
+
+  it "correctly identifies NO substrings found" do
+
+  end
+
+  it "correctly identifies MULTIPLE instances of the same substring" do
+
+  end
+
+end
