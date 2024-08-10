8b54746b166da9b35a549290e900cc5083f6a820
Razvan Rotundu <raz.rotundu@gmail.com>
2024-08-10 19:43:21 -0400

    add comments for strategy

--- project/sub_strings.rb
+++ project/sub_strings.rb
@@ -1,3 +1,14 @@
 # Razvan Rotundu 2024
 # Sub strings project
 
+def sub_strings(string, word_array)
+  out_hash = {}
+
+  word_array.each do |word|
+    # Split the string
+    # Count the spaces + 1
+    # Update out_hash
+    puts("x")
+  end
+  out_hash
+end
