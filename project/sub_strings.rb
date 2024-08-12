d2476bbae0dee8be8a1ee5c3f25495035259d8e5
Razvan Rotundu <raz.rotundu@gmail.com>
2024-08-12 15:08:46 -0400

    Add readme.md, make comparison case-insensitive

--- project/sub_strings.rb
+++ project/sub_strings.rb
@@ -8,7 +8,7 @@ def sub_strings(string, word_array)
   word_array.each do |dict_word|
     split_string = string.split(" ")
     split_string.each do |string_word|
-      if string_word.include?(dict_word)
+      if string_word.downcase.include?(dict_word)
         out_hash[dict_word] = out_hash.fetch(dict_word, 0) + 1
       end
     end
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Odin Project Sub strings
+
+## Overview
+Given a a dictionary of strings and a sentence, this function will return a hash listing which substrings in the dictionary were found in the sentence, and the frequency of their appearance.
+
+Note: Cannot identify repeating substrings within the same word Eg. "is" for Isis
