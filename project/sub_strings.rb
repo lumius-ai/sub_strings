0d8d05669b9a6af4a52c78e81d209ed9882d1e90
Razvan Rotundu <raz.rotundu@gmail.com>
2024-08-12 15:02:02 -0400

    Almost finish project
    add code to count instances of substrings. Does not work if substring words have multiple instances of a dictionary word eg. isis for "is"

--- project/sub_strings.rb
+++ project/sub_strings.rb
@@ -1,14 +1,17 @@
 # Razvan Rotundu 2024
 # Sub strings project
+require 'pry-byebug'
 
 def sub_strings(string, word_array)
   out_hash = {}
 
-  word_array.each do |word|
-    # Split the string
-    # Count the spaces + 1
-    # Update out_hash
-    puts("x")
+  word_array.each do |dict_word|
+    split_string = string.split(" ")
+    split_string.each do |string_word|
+      if string_word.include?(dict_word)
+        out_hash[dict_word] = out_hash.fetch(dict_word, 0) + 1
+      end
+    end
   end
   out_hash
 end
--- spec/sub_strings_spec.rb
+++ spec/sub_strings_spec.rb
@@ -5,20 +5,24 @@
 require_relative "../project/sub_strings"
 
 describe "Sub strings project testing" do
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
   it "Correctly identifies if a substring is present" do
-
+    expect(sub_strings("go", dictionary)).to eq({"go" => 1 })
   end
 
   it "Correctly identifies multiple substrings present" do
+    expect(sub_strings("below", dictionary)).to eq({ "below" => 1, "low" => 1 })
 
   end
 
   it "correctly identifies NO substrings found" do
+    expect(sub_strings("xxx", dictionary)).to eq({})
 
   end
 
-  it "correctly identifies MULTIPLE instances of the same substring" do
+  xit "correctly identifies MULTIPLE instances of the same substring" do
+    expect(sub_strings("lowlow", dictionary)).to eq({"low" => 2})
 
   end
 
